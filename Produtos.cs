361d2dd90c831e898d8e06bc7c40e96a3ae6ace8
DaviihRodrigueSs <davi.devrodrigues@gmail.com>
2024-03-04 17:56:02 -0300

    Construtores

---
+++ Produtos.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Globalization;
+
+
+namespace TrabalhandoComClassesMelhoria
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //Construtor a baixo
+        public Produto(string nome,double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        //void significa vazio
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+        //sobreposição
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
